a1f8c76387778fa176d854efdd49981881871e7f
João Rodrigues <four4kcod@gmail.com>
2025-01-06 09:47:03 -0300

    Aprendizados DOM

---
+++ main/assets/estudos.js
@@ -0,0 +1,28 @@
+// tipos de variáveis:
+let idade =  90 //type number
+let nome =   "João" //type string
+let logado = true //type boolean
+
+// array e obejtos:
+let ingredientes = ["leite", "farinha", "ovo", "corante"]
+let personagem = {
+  nome: "ciclano",
+  nivel: 4,
+  forca: 400,
+  magia: 401,
+  vida:  402,
+  mana:  403
+}
+
+
+// funções
+function somar(a, b) {
+  let resultado = a + b
+  return resultado
+}
+
+let somar2 = (a, b) => {
+  let resultado = a + b
+  return resultado
+}
+
